e934d0cac3d33ab0010df2a698e014ab6edb7f62
Alvarofb7 <alvarofb1998@gmail.com>
2019-03-21 19:21:41 +0100

    Practicas21/03/19

---
+++ Practica9/src/Ejercicio2/TestProblemaTspAG.java
@@ -0,0 +1,43 @@
+package Ejercicio2;
+
+import java.util.List;
+
+import us.lsi.ag.IndexChromosome;
+import us.lsi.ag.IndexProblemAG;
+import us.lsi.ag.agchromosomes.AlgoritmoAG;
+import us.lsi.ag.agchromosomes.ChromosomeFactory.ChromosomeType;
+import us.lsi.ag.agstopping.StoppingConditionFactory;
+import us.lsi.ag.agstopping.StoppingConditionFactory.StoppingConditionType;
+import us.lsi.grafos.datos.Ciudad;
+
+public class TestProblemaTspAG {
+
+	public static void main(String[] args) {
+		setCondiciones();
+		
+		IndexProblemAG<List<Ciudad>> problem = new ProblemaTspAG("./ficheros/TSP.txt");
+		AlgoritmoAG<IndexChromosome> alg = AlgoritmoAG.<IndexChromosome>create(ChromosomeType.IndexPermutation, problem);
+		alg.ejecuta();
+		
+		IndexChromosome mejorSolucion = alg.getBestChromosome();
+		System.out.println("=================================");
+		System.out.println("Camino: " + problem.getSolucion(mejorSolucion));
+		System.out.println("Coste: " + problem.fitnessFunction(mejorSolucion) * -1);
+		System.out.println("=================================");
+		
+	}
+
+	private static void setCondiciones() {
+		// Condiciones "evolutivas"
+		AlgoritmoAG.ELITISM_RATE = 0.2;
+		AlgoritmoAG.CROSSOVER_RATE = 0.8;
+		AlgoritmoAG.MUTATION_RATE = 0.0;
+		AlgoritmoAG.POPULATION_SIZE = 300;
+		
+		StoppingConditionFactory.NUM_GENERATIONS = 1000;
+		StoppingConditionFactory.SOLUTIONS_NUMBER_MIN = 3;
+		StoppingConditionFactory.FITNESS_MIN = 0.;
+		StoppingConditionFactory.stoppingConditionType = StoppingConditionType.SolutionsNumber;
+	}
+
+}
---
+++ PracticaIndividual3_2/src/Ejercicio2/Test.java
@@ -0,0 +1,19 @@
+package Ejercicio2;
+
+import java.util.Arrays;
+
+import us.lsi.lpsolve.solution.AlgoritmoPLI;
+import us.lsi.lpsolve.solution.SolutionPLI;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		SolutionPLI alg = AlgoritmoPLI.getSolutionFromFile("./ficheros/Ejercicio2.txt");
+		System.out.println("Valor resultante de la Función Objetivo: " + alg.getGoal());
+		System.out.println("Valores variables: " + Arrays.toString(alg.getSolution()));
+		
+
+	}
+
+}
